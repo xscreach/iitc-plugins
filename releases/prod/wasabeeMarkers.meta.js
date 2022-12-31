5a3949d3503ff73c927fae0da9a11ae7f3bc9ba9
xscreach <xscreach@users.noreply.github.com>
2022-12-31 20:50:39 +0100

    styling howfix

--- releases/prod/wasabeeMarkers.meta.js
+++ releases/prod/wasabeeMarkers.meta.js
@@ -2,7 +2,7 @@
 // @id            WasabeeAutoMarkers
 // @name          Wasabee Auto Markers
 // @namespace     https://github.com/IITC-CE/ingress-intel-total-conversion
-// @version       0.0.4.20221231192404-8ca120b
+// @version       0.0.4.20221231194940-3b10135
 // @updateURL     https://raw.githubusercontent.com/xscreach/iitc-plugins/master/releases/prod/wasabeeMarkers.meta.js
 // @downloadURL   https://raw.githubusercontent.com/xscreach/iitc-plugins/master/releases/prod/wasabeeMarkers.user.js
 // @description   Marks specific portals with Wasabee markers based on configuration
